500dd445ee33f65afc29a2a9e6f5a04855c6af6d
Lalit Garghate <lalit.garghate@gmail.com>
2020-11-13 13:04:40 +0530

    reducers

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ function buyCake() {
     info: "First redux action"
   };
 }
+
+//Reducers
+
+const initialState = {
+  noOfCakes: 10
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        noOfCakes: state.noOfCakes - 1
+      };
+    default:
+      return state;
+  }
+};
