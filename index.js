0379f4b4348624d3fface029499980b81fec04ef
Lalit Garghate <lalit.garghate@gmail.com>
2020-11-13 12:53:55 +0530

    actions

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(`From Index.js`);
+//define constant that indicates type of action
+const BUY_CAKE = "BUY_CAKE";
+
+//define action - object with type property
+//define action creator - buyCake()
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action"
+  };
+}
